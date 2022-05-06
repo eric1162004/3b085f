a5322b6b25b8d8bd2edc536820d82050e4d13a6e
Eric Cheung <eric1162004@hotmail.com>
2022-05-06 08:00:28 +0000

    wire redesign db association

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,30 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const UserConversation = require("./user_conversation");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.hasMany(Message);
+Message.belongsTo(User);
+
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+
+// Super Many-to-Many relationship
+
+User.belongsToMany(Conversation, { through: UserConversation });
+Conversation.belongsToMany(User, { through: UserConversation });
+
+User.hasMany(UserConversation);
+UserConversation.belongsTo(User);
+
+Conversation.hasMany(UserConversation);
+UserConversation.belongsTo(Conversation);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  UserConversation,
 };
